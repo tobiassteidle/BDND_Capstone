fcf146fcb1837ca390f1438170bffae1c0ec877b
Tobias Steidle <tobias.steidle@softwaredev.de>
2019-03-27 11:20:19 +0100

    feat: combine SolnSquareVerifier and SquareVerifier

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -3,41 +3,53 @@ pragma solidity >=0.4.21 <0.6.0;
 import './ERC721Mintable.sol';
 import "./Verifier.sol";
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+// define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 contract SquareVerifier is Verifier {
-
 }
-
-
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 contract SolnSquareVerifier is REstXERC721Token {
 
-}
+    // TODO define a solutions struct that can hold an index & an address
+    struct Solutions {
+        uint256 index;
+        address from;
+    }
 
+    // define an array of the above struct
+    Solutions [] private solutions;
 
-
-// TODO define a solutions struct that can hold an index & an address
-
-
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
+    // TODO define a mapping to store unique solutions submitted
 
 
 
 // TODO Create an event to emit when a solution is added
+    event SolutionAdded(uint256 indexed index, address indexed from);
 
 
 
 // TODO Create a function to add the solutions to the array and emit the event
 
+    SquareVerifier public verifier;
+
+    constructor (address verifier_address) public {
+        verifier = SquareVerifier(verifier_address);
+    }
 
 
 // TODO Create a function to mint new NFT only after the solution has been verified
 //  - make sure the solution is unique (has not been used before)
 //  - make sure you handle metadata as well as tokenSuplly
+    function mintUniqueTokenTo(address to, uint256 tokenId) public {
+        super.mint(to, tokenId);
+    }
+}
+
+
+
+
+
+
 
 
 
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,6 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
 
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+const proof = require('./proof');
+
+
